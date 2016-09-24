d8edc4a3b66ff0133799889fe09c422806ce39af
sekharvarma <mandativarma@gmail.com>
2016-09-24 18:30:40 +0530

    add new line from eclipse

--- App1.java
+++ App1.java
@@ -2,8 +2,6 @@ class App1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
-		System.out.println("modified Hello World!");
-	     System.out.println("modified2 Hello World!");
+		System.out.println("Hy this is eclipse");
 	}
 }
